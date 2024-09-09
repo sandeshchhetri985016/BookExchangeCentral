4360b51eb0a2b4ebe482e706d5173fb273b56aa9
sandeshchhetri985016 <985016@win.edu.au>
2024-09-09 20:28:52 +1000

    added footer php

---
+++ php/footer.php
@@ -0,0 +1,12 @@
+</main>
+    <footer>
+        <p>© 2024 BookExchangeCentral. All rights reserved.</p>
+        <div class="social-links">
+            <a href="#" aria-label="LinkedIn"><img src="linkedin-icon.png" alt="LinkedIn"></a>
+            <a href="#" aria-label="Facebook"><img src="facebook-icon.png" alt="Facebook"></a>
+            <a href="#" aria-label="Twitter"><img src="twitter-icon.png" alt="Twitter"></a>
+        </div>
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
